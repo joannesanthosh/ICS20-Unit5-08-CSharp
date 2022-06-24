f4bff22598861855a1ab67e71906e6ff6fd7a426
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-24 16:57:12 +0000

    fix whitespace formatting

--- main.cs
+++ main.cs
@@ -7,46 +7,45 @@ using System;
 
 class Program
 {
-  public static void Main(string[] args)
-  {
-    var total = 0;
-    var firstInteger = int.Parse(Console.ReadLine());
-    var secondInteger = int.Parse(Console.ReadLine());
-    var remainder = firstInteger;
-    if (firstInteger < 0 || secondInteger < 0) {
-    Console.WriteLine("Please enter a positive number!");
-    Console.WriteLine("Enter the value of the first integer.");
-    firstInteger = (Convert.ToInt32(Console.ReadLine()));
-    Console.Write("Enter the value of the second integer.");
-    secondInteger = (Convert.ToInt32(Console.ReadLine()));
+    public static void Main(string[] args)
+    {
+        var total = 0;
+        var firstInteger = int.Parse(Console.ReadLine());
+        var secondInteger = int.Parse(Console.ReadLine());
+        var remainder = firstInteger;
+        if (firstInteger < 0 || secondInteger < 0) {
+        Console.WriteLine("Please enter a positive number!");
+        Console.WriteLine("Enter the value of the first integer.");
+        firstInteger = (Convert.ToInt32(Console.ReadLine()));
+        Console.Write("Enter the value of the second integer.");
+        secondInteger = (Convert.ToInt32(Console.ReadLine()));
     }
 
-    // process
-    if (firstInteger > 0 && secondInteger > 0) {
-    while (remainder >= secondInteger) {
-      remainder = remainder - secondInteger;
-      total++;
-    } 
-  } else if ((firstInteger < 0) && (secondInteger < 0)) {
-    remainder = Math.Abs(remainder);
-    secondInteger = Math.Abs(secondInteger)
-    ;while (remainder >= secondInteger) {
-      remainder = remainder - secondInteger;
-      total++;
-    }
-  } else {
-    remainder = Math.Abs(remainder);
-    secondInteger = Math.Abs(secondInteger)
-    ;while (remainder >= secondInteger) {
-      remainder = remainder - secondInteger;
-      total++;
-    }
-    total = -Math.Abs(total);
+        // process
+        if (firstInteger > 0 && secondInteger > 0) {
+          while (remainder >= secondInteger) {
+          remainder = remainder - secondInteger;
+          total++;
+      } 
+    }   else if ((firstInteger < 0) && (secondInteger < 0)) {
+          remainder = Math.Abs(remainder);
+          secondInteger = Math.Abs(secondInteger)
+          ;while (remainder >= secondInteger) {
+          remainder = remainder - secondInteger;
+          total++;
+      }
+    }   else {
+          remainder = Math.Abs(remainder);
+          secondInteger = Math.Abs(secondInteger)
+          ;while (remainder >= secondInteger) {
+          remainder = remainder - secondInteger;
+          total++;
+      }
+          total = -Math.Abs(total);
     };
-    Console.WriteLine("");
-    Console.WriteLine(firstInteger + " ÷ " + secondInteger + " = " + total + " The remainder is " + remainder);
-    Console.WriteLine("\nDone.");
-  } 
+          Console.WriteLine("");
+          Console.WriteLine(firstInteger + " ÷ " + secondInteger + " = " + total + " The remainder is " + remainder);
+          Console.WriteLine("\nDone.");
+    } 
 }
-
     
